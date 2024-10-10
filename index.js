7b8ae175c6882ed49662eb9545fb55d037826424
JBilly77 <nezibills@hotmail.com>
2024-10-10 00:59:11 -0400

    method POST added to the form to show content after insertion

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import morgan from 'morgan';
 
+
 const app = express();
 const PORT = 4000;
 
@@ -11,6 +12,7 @@ app.set('views', './views');
 // middleware
 app.use(express.static('public'));
 app.use(morgan('dev')); // logger
+app.use(express.urlencoded({ extended: true }));
 
 // routes
 app.get('/', (req, res) => {
@@ -18,13 +20,18 @@ app.get('/', (req, res) => {
 });
 
 app.get('/catalog', (req, res) => {
-    res.render('catalog')
+    res.render('catalog', {name: 'Love'});
 });
 
 app.get('/about', (req, res) => {
     res.render('about')
 });
 
+app.post('/catalog', (req, res) => {
+    const caText = req.body.text1
+    res.render('catalog', {text1:caText});
+    console.log("Success!");
+});
 
 
 
